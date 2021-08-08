008d977b6eae828563628b0759cec43fe09adffa
HectorSin <77152104+HectorSin@users.noreply.github.com>
2021-08-08 18:19:04 +0900

    CSS format applied

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_key = "ce2747dc600b24d80c5509a95179bdb2";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `위치 : ${data.name}`;
+      weather.innerText = `현재 날씨 : ${data.weather[0].main} / 기온 : ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
